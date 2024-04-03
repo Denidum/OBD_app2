56d89d83689f0a71e1f5d7f7ae61100662fb0e82
Tanya <vorobest5@gmail.com>
2024-04-03 18:46:37 +0300

    Backend for start

--- db.py
+++ db.py
@@ -3,6 +3,11 @@ import sqlite3
 connect = sqlite3.connect('baza/list.db')
 cursor = connect.cursor()
 
+#Type
+#integer-number
+#text-string
+
+
 #Створити aдаптовану таблицю з 1 колонкою
 def db_plus1(name_table ,name1, type1):
     cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""(""" + name1 + type1 + """) """)
@@ -11,46 +16,46 @@ def db_plus1(name_table ,name1, type1):
 #Створити aдаптовану таблицю з 2 колонкою
 def db_plus2(name_table, name1, type1, name2, type2):
     cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
-                   + name1 + type1 + """, """
-                   + name2 + type2 + """) """)
+                   + name1 +""" """+ type1 + """, """
+                   + name2 +""" """+ type2 + """) """)
     connect.commit()
 
 #Створити адаптовану таблицю для 3 колонок
 def db_plus3(name_table, name1, type1, name2, type2, name3, type3):
     cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
-                   + name1 + type1 + """, """
-                   + name2 + type2 + """, """
-                   + name3 + type3 + """) """)
+                   + name1 +""" """+ type1 + """, """
+                   + name2 +""" """+ type2 + """, """
+                   + name3 +""" """+ type3 + """) """)
     connect.commit()
 
 #Створити адаптовану таблицю для 4 колонок
 def db_plus4(name_table, name1, type1, name2, type2, name3, type3, name4, type4):
     cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
-                   + name1 + type1 + """, """
-                   + name2 + type2 + """, """
-                   + name3 + type3 + """, """
-                   + name4 + type4 + """) """)
+                   + name1 +""" """+ type1 + """, """
+                   + name2 +""" """+ type2 + """, """
+                   + name3 +""" """+ type3 + """, """
+                   + name4 +""" """+ type4 + """) """)
     connect.commit()
 
 #Створити адаптовану таблицю для 5 колонок
 def db_plus5(name_table, name1, type1, name2, type2, name3, type3, name4, type4, name5, type5):
     cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
-                   + name1 + type1 + """, """
-                   + name2 + type2 + """, """
-                   + name3 + type3 + """, """
-                   + name4 + type4 + """, """
-                   + name5 + type5 + """) """)
+                   + name1 +""" """+ type1 + """, """
+                   + name2 +""" """+ type2 + """, """
+                   + name3 +""" """+ type3 + """, """
+                   + name4 +""" """+ type4 + """, """
+                   + name5 +""" """+ type5 + """) """)
     connect.commit()
 
 #Створити адаптовану таблицю для 6 колонок
 def db_plus6(name_table, name1, type1, name2, type2, name3, type3, name4, type4, name5, type5, name6, type6):
     cursor.execute(""" CREATE TABLE IF NOT EXISTS """+name_table+"""("""
-                   + name1 + type1 + """, """
-                   + name2 + type2 + """, """
-                   + name3 + type3 + """, """
-                   + name4 + type4 + """, """
-                   + name5 + type5 + """, """
-                   + name6 + type6 + """) """)
+                   + name1 +""" """+ type1 + """, """
+                   + name2 +""" """+ type2 + """, """
+                   + name3 +""" """+ type3 + """, """
+                   + name4 +""" """+ type4 + """, """
+                   + name5 +""" """+ type5 + """, """
+                   + name6 +""" """+ type6 + """) """)
     connect.commit()
 
 #Створити адаптовану таблицю для 7 колонок
@@ -65,31 +70,27 @@ def db_plus6(name_table, name1, type1, name2, type2, name3, type3, name4, type4,
                    + name7 + type7 + """) """)
     connect.commit()
 
-
 #Видалити таблицю
 def db_delete(name_table):
     cursor.execute("DROP TABLE "+name_table)
     connect.commit()
 
 #Занести дані у таблицю
-def insert_data_db(name_table, data_column, size_data_column):
-    for i in size_data_column:
-        if i==0:
-            cursor.execute("INSERT INTO "+name_table+" VALUES(?);", data_column)
-        elif i==1:
-            cursor.execute("INSERT INTO "+name_table+" VALUES(?,?);", data_column)
-        elif i==2:
-            cursor.execute("INSERT INTO "+name_table+" VALUES(?,?,?);", data_column)
-        elif i==3:
-            cursor.execute("INSERT INTO "+name_table+" VALUES(?,?,?,?);", data_column)
-        elif i==4:
-            cursor.execute("INSERT INTO "+name_table+" VALUES(?,?,?,?,?);", data_column)
-        elif i==5:
-            cursor.execute("INSERT INTO "+name_table+" VALUES(?,?,?,?,?,?);", data_column)
-        elif i==6:
-            cursor.execute("INSERT INTO "+name_table+" VALUES(?,?,?,?,?,?,?);", data_column)
-        else:
-            return 0
+def insert_data_db(name_table, data_column, size_data_column, id_row=0):
+    if size_data_column==0:
+        cursor.execute("INSERT INTO "+name_table+" VALUES(?);", data_column)
+    elif size_data_column==1:
+        cursor.execute("INSERT INTO "+name_table+" VALUES(?,?);", data_column)
+    elif size_data_column==2:
+        cursor.execute("INSERT INTO "+name_table+" VALUES(?,?,?);", data_column)
+    elif size_data_column==3:
+        cursor.execute("INSERT INTO "+name_table+" VALUES(?,?,?,?);", data_column)
+    elif size_data_column==4:
+        cursor.execute("INSERT INTO "+name_table+" VALUES(?,?,?,?,?);", data_column)
+    elif size_data_column==5:
+        cursor.execute("INSERT INTO "+name_table+" VALUES(?,?,?,?,?,?);", data_column)
+    elif size_data_column==6:
+        cursor.execute("INSERT INTO "+name_table+" VALUES(?,?,?,?,?,?,?);", data_column)
     connect.commit()
 
 #Винести всі дані з таблиці
@@ -99,14 +100,35 @@ def select_all_data_db(name_table):
 
 #Винести рядок з таблиці
 def select_data_db(name_table, id_row, number_column):
-    cursor.execute("SELECT * FROM"+name_table)
+    cursor.execute("SELECT * FROM "+name_table)
     while True:
         next_row = cursor.fetchone()
         if next_row:
             dict_data = {}
             for i in range(number_column):
-                if i == number_column:
-                    dict_data[i] = next_row
+                if id_row == i:
+                    dict_data[id_row] = next_row
+                    break
+
+#Спеціальна функція перевірки для таблиці авторизації(human_list)
+def check_info_human(name_human='', passw_human=''):
+    cursor.execute("SELECT * FROM human_list")
+    while True:
+        next_row = cursor.fetchone()
+        if next_row:
+            (id, login, passw) = next_row
+            if name_human == login and passw_human == passw:
+                return "Correct"
+            if name_human == '' and passw_human == '':
+                return "Enter something"
+            if name_human == '':
+                return "Enter login"
+            if passw_human == '':
+                return "Enter password"
+            else:
+                pass
+        else:
+            return "Wrong"
 
 '''
     dict_data = {}
@@ -124,5 +146,16 @@ def select_data_db(name_table, id_row, number_column):
 '''
 
 #Редагувати таблицю
-def alter_data_db():
+def alter_data_db(id_column, id_row, number_column):
+    cursor.execute("SELECT * FROM expenses")
+    while True:
+        next_row = cursor.fetchone()
+        if next_row:
+            dict_data = {}
+            for i in range(number_column):
+                if id_row == i:
+                    dict_data[id_row] = next_row
+                    break
+    insert_data_db()
+
     return 0
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import db ## бібліотека для бази даних
 
-def print_hi():
-    db.select_data_db(5)
+def add_baza_human(): #додати людину до списку людей
+    data_name = [1, "Tanya", "1234"] #Тестові дані
+    #db.db_plus3("human_list", "id","integer","login","text","password","text")
+    db.insert_data_db("human_list", data_name, 2)
+
+def check_human():
+    name_test="Tanya"
+    name_test1="Danya"
+    pasww_test="1234"
+    pasww_test1="0123"
+    print(db.check_info_human(name_test, pasww_test1))
 
 if __name__ == '__main__':
-    print_hi()
+    check_human()
+
+
