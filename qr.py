05128b6d4d79288d2b3f947abcb96e6367d49ef1
unknown <dynysiakmykola@gmail.com>
2024-04-26 00:34:33 +0300

    Shufr and deshufr

--- qr.py
+++ qr.py
@@ -1,19 +1,38 @@
-import pyqrcode
+import qrcode
 import png
 import cv2
 
 
 #створює Qr. data - інформація яку шуфруєм, name - назва Qr
-def create_qr(data, name):
-    url = pyqrcode.create(data)
-    url.png(name , scale = 100)
+def create_qr_code(data, filename="qrcode.png"):
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(data)
+    qr.make(fit=True)
 
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
 
-#Читає Qr. data - інформація яку шуфруєм, name - назва Qr
-def read_qr(data, name):
-    url = pyqrcode.create(data)
-    url.png(name, scale = 100)
-    img = cv2.imread(name)
-    det = cv2.QRCodeDetector()
-    val, pts, st_code = det.detectAndDecode(img)
-    print(val)
+    print(f"QR-код збережено у файлі '{filename}'")
+
+def read_qr_code(image_path):
+    image = cv2.imread(image_path)
+    detector = cv2.QRCodeDetector()
+    data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
+    return data
+
+def encryption(id_table, id_client, id_row):
+    a = str(hex(id_table + 5 * id_client))
+    b = str(hex(id_row + 10 * id_client))
+    return a + "." + b
+
+def dencryption(data):
+    a = data.split(".")
+    print(a)
+
+id_table = 4
+id_client = 1
+id_row = 7
+
+create_qr_code(encryption(id_table, id_client, id_row), "QR4.png")
+
+dencryption(read_qr_code("QR4.png"))
