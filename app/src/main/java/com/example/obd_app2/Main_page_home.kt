4bd2c6b4b708bb9001821b3e8d550d1dcb7a278a
Denidum <denidym1803@gmail.com>
2024-05-02 00:05:58 +0300

    Реалізував основу для видалення таблиць у головній сторінці

--- app/src/main/java/com/example/obd_app2/Main_page_home.kt
+++ app/src/main/java/com/example/obd_app2/Main_page_home.kt
@@ -2,6 +2,7 @@ package com.example.obd_app2
 
 import Adapters.TableListItemsAdapter
 import Table_or_data_classes.Table
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,9 +12,13 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.obd_app2.interfaces.ChooseTableToDelete
 import com.example.obd_app2.interfaces.Main_to_secondary_frags
 
-class Main_page_home : Fragment() {
+class Main_page_home : Fragment(), ChooseTableToDelete {
+    private var myInterface: Main_to_secondary_frags? = null
+    private val items = arrayListOf<Table>()
+    private var listTableToDelete = arrayListOf<Int>()
     private var userId: Int? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,22 +33,27 @@ class Main_page_home : Fragment() {
         val v = inflater.inflate(R.layout.fragment_main_page_home, container, false)
 
         val addTableBtn: Button = v.findViewById(R.id.main_home_page_add_table_button)
+        val delTableBtn: Button = v.findViewById(R.id.main_home_page_delete_table_button)
 
         val data = arguments
         userId = data?.getInt("id")
 
         val itemList: RecyclerView = v.findViewById(R.id.main_home_page_table_list)
-        itemList.setItemViewCacheSize(5)
-        val items = arrayListOf<Table>()
+        itemList.setItemViewCacheSize(6)
+
         //Todo: вивести список завдяки функції, що шукає таблиці за id користувача
         items.add(Table(1, "Test Table1", 4, "00:00 1th January 2024"))
         items.add(Table(2, "Test Table2", 4, "00:00 1th January 2024"))
         items.add(Table(3, "Test Table2", 4, "00:00 1th January 2024"))
         items.add(Table(4, "Test Table2", 4, "00:00 1th January 2024"))
 
+        //На основі кількості таблиць, які є у користувача, у списку listTableToDelete створюється така сама кількість елементів, які відповідають за стан кнопок вибору у списку, що відображається на екрані
+        for(i in 0..<items.size step 1 ){
+            listTableToDelete.add(0)
+        }
 
         itemList.layoutManager = LinearLayoutManager(activity)
-        itemList.adapter = activity?.let { TableListItemsAdapter(items, it) }
+        itemList.adapter = activity?.let { TableListItemsAdapter(items, it, this) }
 
         val myInterface: Main_to_secondary_frags = activity as Main_to_secondary_frags
         addTableBtn.setOnClickListener {
@@ -54,7 +64,54 @@ class Main_page_home : Fragment() {
                 Toast.makeText(v.context, "There is limit of 5 tables", Toast.LENGTH_SHORT).show()
             }
         }
+
+        delTableBtn.setOnClickListener {
+            if(!listTableToDelete.contains(1)){
+                Toast.makeText(context, "Choose any table to delete", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                confirmDeletionDialog()
+            }
+        }
         return v
+    }
+
+    fun confirmDeletionDialog(){
+        val dialog = AlertDialog.Builder(activity)
+        dialog.setTitle("Confirm deletion")
+        dialog.setTitle("Do you want to delete chosen tables?")
+        dialog.setPositiveButton("No") { dialog, which ->
+
+            dialog.dismiss()
+        }
+        dialog.setNegativeButton("Yes") { dialog, which ->
+            deleteTableFromSQL()
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    fun deleteTableFromSQL(){
+        var rastrelList = arrayListOf<Table>()
+        for(i in 0..<listTableToDelete.size step 1){
+            if(listTableToDelete[i] == 1){
+                rastrelList.add(items[i])
+            }
+        }
+        for(i in 0..<rastrelList.size step 1){
+            //Todo: тут викликаєш бекенд функцію, що видаляє таблицю
+            //у rastrelList зберігають елементи типу Table, звідти витягуєш потрібні дані, щоб видалити таблицю
+        }
+
+    }
+    override fun buttonChecked(index: Int) {
+        listTableToDelete.removeAt(index)
+        listTableToDelete.add(index, 1)
+    }
+
+    override fun buttonUnchecked(index: Int) {
+        listTableToDelete.removeAt(index)
+        listTableToDelete.add(index, 0)
     }
 
 }
